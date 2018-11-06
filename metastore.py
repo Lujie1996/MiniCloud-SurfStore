db496f29be261022279ac884cb2d0c384f08dd77
Jie Lu <lujie1996@hotmail.com>
2018-11-05 17:24:58 -0800

    except properly

--- metastore.py
+++ metastore.py
@@ -149,16 +149,13 @@ class MetadataStore(rpyc.Service):
         if filename in self.filename_hashlist:
             version = self.filename_version[filename]
             hashlist = self.filename_hashlist[filename]
-            # return version, str(hashlist)
-            return version, tuple(hashlist)
+            return version, str(hashlist)
         elif filename in self.tombstone_filename_version:
             version = self.tombstone_filename_version[filename]
-            # hashlist = str(list())
-            hashlist = tuple()
+            hashlist = str(list())
             return version, hashlist
         else:
-            # return 0, str(list())
-            return 0, tuple()
+            return 0, str(list())
 
 
 def parse_config(config):
